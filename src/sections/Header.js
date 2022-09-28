8fbc5ceb5497dfd64e0e9cf1ed99995cb5324abe
Marwan Zaky <marouaneezzaky@gmail.com>
2022-09-28 23:26:34 +0100

    Update Header.js

--- src/sections/Header.js
+++ src/sections/Header.js
@@ -7,17 +7,19 @@ const shopNow = () => {
 // }
 
 function Header() {
-    return <header>
-        <div className="xl:container xl:mx-auto header-box">
-            <h1 className="header-text">Anniversary Gift For Him.</h1>
-            <p className="header-parag">This soft cover journal is a great reminder to never forget how wildly capable you are.</p>
+    return (
+        <header>
+            <div className="xl:container xl:mx-auto header-box">
+                <h1 className="header-text">Anniversary Gift For Him.</h1>
+                <p className="header-parag">This soft cover journal is a great reminder to never forget how wildly capable you are.</p>
 
-            <div className="flex justify-center">
-                <button onClick={shopNow} className="btn-base btn-full">Shop now</button>
-                {/* <button onClick={explore} className="btn-base btn-ghost">Explore</button> */}
+                <div className="flex justify-center">
+                    <button onClick={shopNow} className="btn-base btn-full">Shop now</button>
+                    {/* <button onClick={explore} className="btn-base btn-ghost">Explore</button> */}
+                </div>
             </div>
-        </div>
-    </header>
+        </header>
+    )
 }
 
 export default Header;
