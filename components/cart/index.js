9f96100cbc02c91b2f1dd0a6fb7a107210403802
Marwan Zaky <marouaneezzaky@gmail.com>
2022-10-06 15:27:15 +0100

    Update cart.js

--- components/cart/index.js
+++ components/cart/index.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import Link from 'next/link';
 
 import Settings from '../../utils/settings';
 import CartItems from '../../utils/cartItems';
@@ -10,8 +11,8 @@ function Item(props) {
         <tr className="cart-table-item">
             <th className="cart-table-item-product">
                 <div className="cart-table-item-product-div" >
-                    <a href={'/product/' + props.id} className='cart-table-item-product-img'><img src={`${Settings.server}/${props.src}`} alt={props.alt} /></a>
-                    <a href={'/product/' + props.id} className='cart-table-item-product-name'>{props.name}</a>
+                    <Link href={'/product/' + props.id}><a className='cart-table-item-product-img'><img src={`${Settings.server}/${props.src}`} alt={props.alt} /></a></Link>
+                    <Link href={'/product/' + props.id}><a className='cart-table-item-product-name'>{props.name}</a></Link>
                 </div>
             </th>
             <th className="cart-table-item-price">{'$' + props.price / 100}</th>
@@ -26,60 +27,70 @@ function Item(props) {
     )
 }
 
-function Cart() {
-    const [cartItems, setCartItems] = useState(null);
+class Cart extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            cartItems: null
+        };
+    }
 
-    useEffect(() => {
-        setCartItems(CartItems);
-    });
+    componentDidMount() {
+        this.setState({
+            cartItems: CartItems
+        });
+    }
 
-    useEffect(() => {
-        if (cartItems)
-            updateCartSubtotal();
-    }, cartItems);
+    componentDidUpdate() {
+        updateCartSubtotal();
+    }
 
-    if (!cartItems)
-        return <></>
+    render() {
+        const { cartItems } = this.state;
 
-    return (
-        <section className="section-cart">
-            <div className="xl:container xl:mx-auto">
-                <h2>Your cart</h2>
+        if (!cartItems)
+            return <></>
 
-                <div className="cart-container">
-                    <table className="cart-table">
-                        <thead className='cart-table-thead'>
-                            <tr>
-                                <th className='cart-table-thead-th-product text-left'>Product</th>
-                                <th className='cart-table-thead-th-price'>Price</th>
-                                <th className='cart-table-thead-th-quantity'>Quantity</th>
-                                <th className='cart-table-thead-th-total'>Total</th>
-                                <th className='cart-table-thead-th-remove'></th>
-                            </tr>
-                        </thead>
+        return (
+            <section className="section-cart">
+                <div className="xl:container xl:mx-auto">
+                    <h2>Your cart</h2>
 
-                        <tbody>
-                            {cartItems.items.map(el => <Item key={el.id} id={el.id} src={el.pictures[0]} name={el.name} alt={el.name} price={el.price} />)}
-                        </tbody>
-                    </table >
+                    <div className="cart-container">
+                        <table className="cart-table">
+                            <thead className='cart-table-thead'>
+                                <tr>
+                                    <th className='cart-table-thead-th-product text-left'>Product</th>
+                                    <th className='cart-table-thead-th-price'>Price</th>
+                                    <th className='cart-table-thead-th-quantity'>Quantity</th>
+                                    <th className='cart-table-thead-th-total'>Total</th>
+                                    <th className='cart-table-thead-th-remove'></th>
+                                </tr>
+                            </thead>
 
-                    <div className="cart-subtotal">
-                        <div className="cart-subtotal-div">
-                            <span className="cart-subtotal-div-title">Subtotal</span>
-                            <span className="cart-subtotal-div-price">$0 USD</span>
+                            <tbody>
+                                {cartItems.items.map(el => <Item key={el.id} id={el.id} src={el.pictures[0]} name={el.name} alt={el.name} price={el.price} />)}
+                            </tbody>
+                        </table >
+
+                        <div className="cart-subtotal">
+                            <div className="cart-subtotal-div">
+                                <span className="cart-subtotal-div-title">Subtotal</span>
+                                <span className="cart-subtotal-div-price">$0 USD</span>
+                            </div>
+
+                            <div className="cart-subtotal-note">Taxes and shipping calculated at checkout</div>
                         </div>
 
-                        <div className="cart-subtotal-note">Taxes and shipping calculated at checkout</div>
-                    </div>
+                        <div className="cart-item-checkout">
+                            <button className='btn-base btn-full cart-item-checkout-btn' onClick={() => checkout()}>Check out</button>
+                        </div>
 
-                    <div className="cart-item-checkout">
-                        <button className='btn-base btn-full cart-item-checkout-btn' onClick={() => checkout()}>Check out</button>
                     </div>
-
                 </div>
-            </div>
-        </section >
-    )
+            </section >
+        )
+    }
 }
 
 export default Cart;
