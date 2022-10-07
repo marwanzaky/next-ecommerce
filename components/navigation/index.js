8679612efa8a4624ee543c0c147c5e751f18f925
Marwan Zaky <marouaneezzaky@gmail.com>
2022-10-07 14:45:55 +0100

    Update navigation

--- components/navigation/index.js
+++ components/navigation/index.js
@@ -1,70 +1,18 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Link from 'next/link';
 
 import CartItems from '../../utils/cartItems';
 import Settings from '../../utils/settings';
 
 function Nav(props) {
-    return (
-        <li>
-            <Link href={props.href}>
-                <a>{props.name}</a>
-            </Link>
-        </li>
-    )
-}
-
-function ButtonCart() {
-    const [cartItemsLength, setCartItemsLength] = useState(0);
-
-    const buttonCartLengthStyle = {
-        display: 'none'
-    }
-
-    if (cartItemsLength > 0)
-        buttonCartLengthStyle.display = 'flex';
-
-    useEffect(() => {
-        setCartItemsLength(CartItems.items.length);
-    });
-
-    return (
-        <Link href='/cart'>
-            <a className='nav-btn nav-btn-icon'>
-                <span className="material-symbols-outlined">shopping_cart</span>
-                <div className='nav-button-cart-length' style={buttonCartLengthStyle}>{cartItemsLength}</div>
-            </a>
+    return <li>
+        <Link href={props.href}>
+            <a>{props.name}</a>
         </Link>
-    )
-}
-
-function Navigation() {
-    return (
-        <nav>
-            <div className='main-nav-promo'>Welcome to our Store!</div>
-            <div className="xl:container xl:mx-auto p-5 main-nav-box">
-                <Link href='/'><a className='logo'>{Settings.name}</a></Link>
-
-                <ul className='main-nav'>
-                    <Nav href='/' name='Home' />
-                    <Nav href='/products' name='Shop' />
-                    <Nav href='/about' name='About' />
-                    <Nav href='/contact' name='Contact' />
-                </ul>
-
-                <div>
-                    <NavBtn href='/signin' icon='person' />
-                    <ButtonCart />
-                </div>
-            </div>
-        </nav >
-    )
+    </li>
 }
 
 function NavBtn(props) {
-    {/* <a href='/signin' className='nav-btn' ><span className="material-symbols-outlined">person</span></a> */ }
-    {/* <a href='/favourite' className='button-icon' ><span className="material-symbols-outlined">favorite</span></a> */ }
-
     return (
         <Link href={props.href}>
             <a className='nav-btn' >
@@ -72,6 +20,60 @@ function NavBtn(props) {
             </a>
         </Link>
     )
+}
+
+class NavBtnCart extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            cartItemsLength: 0
+        };
+    }
+
+    componentDidMount() {
+        this.setState({
+            cartItemsLength: CartItems.items.length
+        });
+    }
+
+    render() {
+        const { cartItemsLength } = this.state;
+
+        const buttonCartLengthStyle = {
+            display: 'none'
+        }
+
+        if (cartItemsLength > 0)
+            buttonCartLengthStyle.display = 'flex';
+
+        return <Link href='/cart'>
+            <a className='nav-btn nav-btn-icon'>
+                <span className="material-symbols-outlined">shopping_cart</span>
+                <div className='nav-button-cart-length' style={buttonCartLengthStyle}>{cartItemsLength}</div>
+            </a>
+        </Link>
+    }
+}
+
+function Navigation() {
+    return <nav>
+        <div className='main-nav-promo'>Welcome to our Store!</div>
+        <div className="xl:container xl:mx-auto p-5 main-nav-box">
+            <Link href='/'><a className='logo'>{Settings.name}</a></Link>
+
+            <ul className='main-nav'>
+                <Nav href='/' name='Home' />
+                <Nav href='/products' name='Shop' />
+                <Nav href='/about' name='About' />
+                <Nav href='/contact' name='Contact' />
+            </ul>
+
+            <div>
+                <NavBtn href='/signin' icon='person' />
+                <NavBtnCart />
+            </div>
+        </div>
+    </nav>
 }
 
 export default Navigation;
