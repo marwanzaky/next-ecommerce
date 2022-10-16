28a7f97984601491a89c01c0b07bd80bab4da62b
Marwan Zaky <marouaneezzaky@gmail.com>
2022-10-16 22:49:56 +0100

    Update index.js

--- components/about/index.js
+++ components/about/index.js
@@ -21,8 +21,8 @@ function About() {
             <h2>What is {Settings.name}?</h2>
 
             <div className="grid grid-cols-1 lg:grid-cols-3 gap-5 mb-[50px]">
-                <Paragraph title='A community doing good' description='Storio is a global online marketplace, where people come together to make, sell, buy, and collect unique items. We’re also a community pushing for positive change for small businesses, people, and the planet. Here are some of the ways we’re making a positive impact, together.' />
-                <Paragraph title='Support independent creators' description='There’s no Storio warehouse – just millions of people selling the things they love. We make the whole process easy, helping you connect directly with makers to find something extraordinary.' />
+                <Paragraph title='A community doing good' description={`${Settings.name} is a global online marketplace, where people come together to make, sell, buy, and collect unique items. We’re also a community pushing for positive change for small businesses, people, and the planet. Here are some of the ways we’re making a positive impact, together.`} />
+                <Paragraph title='Support independent creators' description={`There’s no ${Settings.name} warehouse – just millions of people selling the things they love. We make the whole process easy, helping you connect directly with makers to find something extraordinary.`} />
                 <Paragraph title='Peace of mind' description='Your privacy is the highest priority of our dedicated team. And if you ever need assistance, we are always ready to step in for support.' />
             </div>
 
