5a70a775c1b3a1520c4526fb5fa88fdac4ebf4ec
Marwan Zaky <marouaneezzaky@gmail.com>
2022-10-27 15:58:51 +0100

    Update stars.js, and globals.scss

--- utils/components/stars.js
+++ utils/components/stars.js
@@ -1,21 +1,28 @@
 function Star() {
-    return <span className="material-symbols-outlined stars-star-icon">star</span>
+    return <span className="material-symbols-outlined stars-star">star</span>
 }
 
 function StarHalf() {
-    return <span className="material-symbols-outlined stars-star-icon">star_half</span>
+    return <span className="material-symbols-outlined stars-star_half">star_half</span>
+}
+
+function StarBorder() {
+    return <span className="material-symbols-outlined stars-star_border">star</span>
 }
 
 function Stars({ className, total, value = 5, displayTotal = true }) {
     const stars = [];
 
-    for (let i = 0; i < value; i++)
+    for (let i = 0; i < parseInt(value); i++)
         stars.push(<Star />)
 
     if (value % 1 !== 0)
         stars.push(<StarHalf />)
 
-    return <span className={`${className} stars`}>
+    for (let i = 0; i < 5 - Math.ceil(value); i++)
+        stars.push(<StarBorder />)
+
+    return <span className={`stars ${className}`}>
         {stars}{displayTotal ? `\u00A0(${total})` : ''}
     </span>
 }
