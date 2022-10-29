04a976342549aae74e655da0e6bfcc59e287087c
Marwan Zaky <marouaneezzaky@gmail.com>
2022-10-29 11:24:36 +0100

    Minor changes to product page

--- components/product/index.js
+++ components/product/index.js
@@ -19,12 +19,12 @@ function ProductPreview({ product }) {
     return <div className='product-preview'>
         <div className='product_item-save opacity-0'><span className='material-symbols-outlined'>favorite</span></div>
 
-        <Img class_name='product-preview-img img' src={product.imgs ? `${Settings.server}/${product.imgs[img]}` : '/dummy-product.png'} alt={product.name} />
+        <Img class_name='product-preview-img img' src={`${Settings.server}/${product.imgs[img]}`} alt={product.name} />
 
         <div className='product-preview-imgs'>
-            {product.imgs && product.imgs.map((el, i) =>
+            {product.imgs.map((el, i) =>
                 <div key={`${product.name} ${i + 1}`} className='product-preview-imgs-img' onClick={() => previewImg(i)}>
-                    <Img class_name='img' src={`${Settings.server}/` + el} alt={`${product.name} ${i + 1}`} />
+                    <Img class_name='img' src={`${Settings.server}/${el}`} alt={`${product.name} ${i + 1}`} />
                 </div>)}
         </div>
     </div>
