bdd2c919e73dd0cea332f0b52a474c53f92e225e
Marwan Zaky <marouaneezzaky@gmail.com>
2023-01-29 10:20:44 +0100

    Update app/products/page.js

--- app/products/page.js
+++ app/products/page.js
@@ -1,5 +1,16 @@
-export default function Page() {
+import Products from '@components/products';
+
+export default async function Page() {
+    const products = await getProducts();
+
     return <div className='App'>
-        Products
+        <Products products={products} />
     </div>
+}
+
+async function getProducts() {
+    const req = await fetch(`${process.env.NEXT_PUBLIC_SERVER}/products`);
+    const json = await req.json();
+
+    return json.data.products;
 }
