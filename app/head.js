b9b54df7574bc76e4675be7a35fb6f60b94ad085
Marwan Zaky <marouaneezzaky@gmail.com>
2023-02-13 14:29:29 +0100

    Load favicon from the server

--- app/head.js
+++ app/head.js
@@ -1,6 +1,6 @@
 export default function Head() {
     return <>
         <meta name='viewport' content='width=device-width, initial-scale=1' />
-        <link rel='icon' href='/favicon.png' />
+        <link rel='icon' href={`${process.env.NEXT_PUBLIC_SERVER.replace('/api/v1', '')}/favicon.png`} />
     </>
 }
