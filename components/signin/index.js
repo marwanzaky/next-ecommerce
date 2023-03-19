fa5b78c8932085b1dfcc82eb9c67ea6e826af39a
Marwan Zaky <marouaneezzaky@gmail.com>
2023-03-19 23:34:32 +0000

    Update components/signin

--- components/signin/index.js
+++ components/signin/index.js
@@ -30,7 +30,6 @@ export default function Signin() {
     const onSubmit = async event => {
         event.preventDefault();
 
-
         const requestOptions = {
             method: 'POST',
             credentials: 'include',
