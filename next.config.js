dc33d9955bb699dfa23848117781059747a67958
Marwan Zaky <marouaneezzaky@gmail.com>
2024-01-19 20:37:39 +0100

    Install latest version of Next 13

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   images: {
     domains: [process.env.NEXT_PUBLIC_DOMAINS],
     formats: ['image/avif', 'image/webp'],
