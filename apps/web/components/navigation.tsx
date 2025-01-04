1cd2dc9b18250442657ce127d575276e47e50665
Marwan Zaky <marouaneezzaky@gmail.com>
2025-01-04 22:57:51 +0100

    enhance navigation

--- apps/web/components/navigation.tsx
+++ apps/web/components/navigation.tsx
@@ -5,7 +5,7 @@ import { AppDispatch, useAppSelector } from "../redux/store";
 
 import React from "react";
 import { selectUserInitials } from "../redux/selectors/authSelectors";
-import { BadgeCheck, ChevronsUpDown, LogOut, ShoppingCart } from "lucide-react";
+import { BadgeCheck, LogOut, Search, ShoppingCart } from "lucide-react";
 import { Button } from "./ui/button";
 import {
 	DropdownMenu,
@@ -20,15 +20,30 @@ import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import { useDispatch } from "react-redux";
 import { logOut } from "redux/slices/authSlice";
 import { Separator } from "./ui/separator";
+import { cn } from "@/lib/utils";
 
-function NavItem(props: {
-	children: React.ReactNode;
-	onClick?: React.MouseEventHandler<HTMLAnchorElement>;
+function ButtonIcon({
+	className,
+	icon,
+	onClick,
+	children,
+}: {
+	icon: JSX.Element;
+	className?: string;
+	onClick?: React.MouseEventHandler<HTMLButtonElement>;
+	children?: React.ReactNode;
 }) {
 	return (
-		<a className="text-sm font-medium cursor-pointer" onClick={props.onClick}>
-			{props.children}
-		</a>
+		<button
+			className={cn(
+				"w-9 h-9 flex justify-center items-center text-gray-400 rounded-md hover:bg-accent",
+				className,
+			)}
+			onClick={onClick}
+		>
+			{icon}
+			{children}
+		</button>
 	);
 }
 
@@ -46,68 +61,54 @@ export default function Navigation() {
 	const userInitials = useAppSelector(selectUserInitials);
 
 	return (
-		<nav className="sticky top-0 z-10 bg-white dark:bg-black flex justify-between items-center h-16 border-b px-4">
-			<ul className="flex gap-8">
-				<div className="hidden sm:flex">
-					<NavItem onClick={() => router.push("/")}>Home</NavItem>
+		<nav className="sticky top-0 z-10 bg-white dark:bg-black flex items-center h-16 border-b px-4">
+			{/* left */}
+			<div className="flex flex-1 justify-start">
+				<div className="flex gap-8">
+					<button className="font-extrabold" onClick={() => router.push("/")}>
+						Mamolio
+					</button>
+
+					<ul className="flex gap-8">
+						<div className="hidden sm:flex">
+							<NavItem onClick={() => router.push("/")}>Home</NavItem>
+						</div>
+
+						<NavItem onClick={() => router.push("/shop")}>Shop</NavItem>
+
+						<div className="hidden sm:flex gap-8">
+							<NavItem>About</NavItem>
+						</div>
+					</ul>
 				</div>
+			</div>
 
-				<NavItem onClick={() => router.push("/shop")}>Shop</NavItem>
+			{/* center */}
+			<div className="flex flex-2 justify-center"></div>
 
-				<div className="hidden sm:flex gap-8">
-					<NavItem>About</NavItem>
-				</div>
-			</ul>
+			{/*  right */}
+			<div className="flex flex-1 justify-end">
+				{isAuthenticated ? (
+					<div className="flex items-center gap-2">
+						<ButtonIcon
+							icon={<Search />}
+							onClick={() => router.push("/shop")}
+						/>
 
-			{isAuthenticated ? (
-				<div className="flex gap-2">
-					<Button
-						className="relative"
-						variant="outline"
-						size="icon"
-						onClick={() => router.push("/cart")}
-					>
-						<ShoppingCart size="1rem" />
-
-						{items.length > 0 && (
-							<span className="absolute -top-1 -right-1 w-4 h-4 text-xs font-bold text-white bg-red-500 rounded-full flex items-center justify-center">
-								{items.length}
-							</span>
-						)}
-					</Button>
-
-					<DropdownMenu>
-						<DropdownMenuTrigger asChild>
-							<div className="flex gap-2 items-center">
-								<Avatar className="h-8 w-8 rounded-lg">
-									<AvatarImage
-										src={user?.photo || undefined}
-										alt={user?.name}
-									/>
-									<AvatarFallback className="rounded-lg">
-										{userInitials}
-									</AvatarFallback>
-								</Avatar>
-
-								<div className="grid flex-1 text-left text-sm leading-tight">
-									<span className="truncate font-semibold">{user?.name}</span>
-									<span className="truncate text-xs">{user?.email}</span>
-								</div>
-
-								<ChevronsUpDown className="ml-auto size-4" />
-							</div>
-						</DropdownMenuTrigger>
-
-						<DropdownMenuContent
-							className="w-[--radix-dropdown-menu-trigger-width] min-w-56 rounded-lg"
-							side="bottom"
-							align="end"
-							sideOffset={4}
+						<ButtonIcon
+							className="relative"
+							icon={<ShoppingCart />}
+							onClick={() => router.push("/cart")}
 						>
-							<DropdownMenuLabel className="p-0 font-normal">
-								<div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
-									<Avatar className="h-8 w-8 rounded-lg">
-										{/* user.avatar */}
+							{items.length > 0 && <CountBadge count={items.length} />}
+						</ButtonIcon>
+
+						<Separator orientation="vertical" className="h-5" />
+
+						<DropdownMenu>
+							<DropdownMenuTrigger asChild>
+								<div className="flex gap-2 items-center">
+									<Avatar className="h-9 w-9 rounded-lg">
 										<AvatarImage
 											src={user?.photo || undefined}
 											alt={user?.name}
@@ -116,45 +117,90 @@ export default function Navigation() {
 											{userInitials}
 										</AvatarFallback>
 									</Avatar>
-
-									<div className="grid flex-1 text-left text-sm leading-tight">
-										<span className="truncate font-semibold">{user?.name}</span>
-										<span className="truncate text-xs">{user?.email}</span>
-									</div>
 								</div>
-							</DropdownMenuLabel>
+							</DropdownMenuTrigger>
 
-							<DropdownMenuSeparator />
+							<DropdownMenuContent
+								className="w-[--radix-dropdown-menu-trigger-width] min-w-56 rounded-lg"
+								side="bottom"
+								align="end"
+								sideOffset={4}
+							>
+								<DropdownMenuLabel className="p-0 font-normal">
+									<div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
+										<Avatar className="h-8 w-8 rounded-lg">
+											<AvatarImage
+												src={user?.photo || undefined}
+												alt={user?.name}
+											/>
+											<AvatarFallback className="rounded-lg">
+												{userInitials}
+											</AvatarFallback>
+										</Avatar>
 
-							<DropdownMenuGroup>
-								<DropdownMenuItem onClick={() => router.push("/settings")}>
-									<BadgeCheck />
-									Account
+										<div className="grid flex-1 text-left text-sm leading-tight">
+											<span className="truncate font-semibold">
+												{user?.name}
+											</span>
+											<span className="truncate text-xs">{user?.email}</span>
+										</div>
+									</div>
+								</DropdownMenuLabel>
+
+								<DropdownMenuSeparator />
+
+								<DropdownMenuGroup>
+									<DropdownMenuItem onClick={() => router.push("/settings")}>
+										<BadgeCheck />
+										Account
+									</DropdownMenuItem>
+								</DropdownMenuGroup>
+
+								<DropdownMenuSeparator />
+
+								<DropdownMenuItem onClick={() => dispatch(logOut())}>
+									<LogOut />
+									Log out
 								</DropdownMenuItem>
-							</DropdownMenuGroup>
+							</DropdownMenuContent>
+						</DropdownMenu>
+					</div>
+				) : (
+					<div className="flex items-center gap-2">
+						<Button variant="ghost" onClick={() => router.push("/login")}>
+							Sign in
+						</Button>
 
-							<DropdownMenuSeparator />
+						<Separator orientation="vertical" className="h-5" />
 
-							<DropdownMenuItem onClick={() => dispatch(logOut())}>
-								<LogOut />
-								Log out
-							</DropdownMenuItem>
-						</DropdownMenuContent>
-					</DropdownMenu>
-				</div>
-			) : (
-				<div className="flex items-center gap-2">
-					<Button variant="ghost" onClick={() => router.push("/login")}>
-						Sign in
-					</Button>
-
-					<Separator orientation="vertical" className="h-5" />
-
-					<Button variant="ghost" onClick={() => router.push("/signup")}>
-						Create account
-					</Button>
-				</div>
-			)}
+						<Button variant="ghost" onClick={() => router.push("/signup")}>
+							Create account
+						</Button>
+					</div>
+				)}
+			</div>
 		</nav>
+	);
+}
+
+function CountBadge({ count }: { count: number }) {
+	return (
+		<div className="absolute -top-1 -right-1 w-4 h-4 text-xs font-bold text-white bg-red-500 rounded-full flex items-center justify-center">
+			{count}
+		</div>
+	);
+}
+
+function NavItem(props: {
+	children: React.ReactNode;
+	onClick?: React.MouseEventHandler<HTMLAnchorElement>;
+}) {
+	return (
+		<a
+			className="text-sm font-medium cursor-pointer flex justify-center items-center"
+			onClick={props.onClick}
+		>
+			{props.children}
+		</a>
 	);
 }
