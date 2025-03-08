3fc4be13e8669775e969ed8e69fcdc794512d0eb
Marwan Zaky <marouaneezzaky@gmail.com>
2025-03-08 00:55:10 +0000

    update return policy text for proper HTML entity usage

--- components/product/index.tsx
+++ components/product/index.tsx
@@ -123,13 +123,14 @@ function Details({ product }: { product: IProduct }) {
 					<br />
 					To be eligible for a return, your item must be in the same condition
 					that you received it, unworn or unused, with tags, and in its original
-					packaging. You'll also need the receipt or proof of purchase.
+					packaging. You&apos;ll also need the receipt or proof of purchase.
 					<br />
 					<br />
 					To start a return, you can contact us at contact@goodies2buy.com. If
-					your return is accepted, we'll send you a return shipping label, as
-					well as instructions on how and where to send your package. Items sent
-					back to us without first requesting a return will not be accepted.
+					your return is accepted, we&apos;ll send you a return shipping label,
+					as well as instructions on how and where to send your package. Items
+					sent back to us without first requesting a return will not be
+					accepted.
 					<br />
 					<br />
 					You can always contact us for any return question at
